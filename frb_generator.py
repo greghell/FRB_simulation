84fcd87a7b9e30ca7954f58392386ff243ba4943
greghell <greg.hellbourg@gmail.com>
2018-11-16 12:17:13 +0800

    Update frb_generator.py

--- frb_generator.py
+++ frb_generator.py
@@ -6,13 +6,26 @@ FRB generator
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def myspectro(sig,nResol,f_start,f_stop):
+    nSam = np.shape(signal)[0];
+    nTime = int(np.floor(nSam/nResol));
+    sig = np.reshape(sig[0:nTime*nResol],(nResol,nTime), order='F');
+    sig = np.abs(np.fft.fft(sig,axis=0))**2;
+    BW = f_stop - f_start;
+    plt.figure();
+    plt.imshow(10.*np.log10(sig),extent=[0,float(nSam)/float(BW),f_stop,f_start],aspect='auto');
+    plt.xlabel('time [s]');
+    plt.ylabel('frequency [MHz]');
+    plt.title('spectrogram [dB]');
+
 f_start = 100.       # start frequency
 f_stop = 1000.       # stop frequency
-DM = 500.            # dispersion measure
+DM = 150.            # dispersion measure
 SNR = 20.            # SNR in dB
 nFiltSize = 2.**10   # size of FRB filter
 alpha = 1.           # FRB curvature index
-frbwidth = 100.     # frb instantaneous frequency bandwidth index
+frbwidth = 100.      # frb instantaneous frequency bandwidth index
 
 
 BW = f_stop - f_start   # total signal bandwidth
@@ -23,8 +36,8 @@ t_d_samples = int(round(abs(t_d*BW)))    # number of samples corresponding to t_
 nTotSam = int(round(t_d_samples/0.6))
 
 if t_d_samples + round(nTotSam/10) >= nTotSam:
-    print 'increase number of samples'
-    print 'need at least ' + str(t_d_samples + round(nTotSam/10)) + ' samples in total'
+    print('increase number of samples')
+    print('need at least ' + str(t_d_samples + round(nTotSam/10)) + ' samples in total')
     exit()
 
 t = np.arange(int(t_d_samples),dtype = 'float')     # time....
@@ -39,4 +52,4 @@ signal = (np.random.normal(0.,1.,int(nTotSam))+1j*np.random.normal(0.,1.,int(nTo
 # background noise + FRB starting at 10%
 signal[int(round(nTotSam/10)):int(round(nTotSam/10)+t_d_samples)] = puls + signal[int(round(nTotSam/10)):int(round(nTotSam/10)+t_d_samples)]
 
-plt.specgram(signal,1024)
+myspectro(signal,1024,f_start,f_stop)
